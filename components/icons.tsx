985ceba835dc98680fd90c7c65aa77d1ae930900
in-deepaknegi <negideepak.in@gmail.com>
2024-11-07 18:23:17 +0530

    updated about section

--- components/icons.tsx
+++ components/icons.tsx
@@ -98,7 +98,7 @@ export function Whatsapp(props: React.SVGProps<SVGSVGElement>) {
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
         >
-            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
             <g
                 id="SVGRepo_tracerCarrier"
                 strokeLinecap="round"
--- components/routes/home/about.tsx
+++ components/routes/home/about.tsx
@@ -20,6 +20,9 @@ const About: React.FC<CtaProps> = (props) => {
         offset: ['start end', 'end start']
     })
 
+    const i1 = images.slice(0, 5);
+    const i2 = images.slice(6, 11);
+
     return (
         <section ref={container} className="relative w-full py-24 font-insr">
 
@@ -35,8 +38,8 @@ const About: React.FC<CtaProps> = (props) => {
 
             <div className="overflow-hidden mt-28">
                 <div ref={container} className="">
-                    <Slide direction={'left'} progress={scrollYProgress} images={images} />
-                    <Slide direction={'right'} progress={scrollYProgress} images={images} />
+                    <Slide direction={'left'} progress={scrollYProgress} images={i1} />
+                    <Slide direction={'right'} progress={scrollYProgress} images={i2} />
 
                 </div>
             </div>
@@ -45,10 +48,11 @@ const About: React.FC<CtaProps> = (props) => {
                 <div className="flex w-full flex-col gap-4 text-left">
 
                     <h2 className="mt-6 font-oggr text-4xl md:text-6xl">
-                        Effortlessly sustainable. <br />
-                        The perfect living space.
+                        Why Sea Hawk Adventures?
                     </h2>
-                    <p className="mt-auto max-w-md text-2xl">{description}</p>
+                    <p className="mt-auto max-w-4xl text-lg">
+                        Being the flag bearer of river rafting, hiking, mountain biking, and wildlife safari in the Indian adventure circuit is a source of pride and excitement for us. Our rafting crew has completed IRF and Rescue 3 training. Our hiking squad is made up of graduates from India&apos;s most prestigious mountaineering schools. As a responsible adventure trip company, we are proud of our safety standards and our desire to remain India&apos;s number one adventure tour operator.
+                    </p>
                     <Link
                         href={"#"}
                         className="flex text-lg items-center gap-1 w-fit rounded-sm bg-blue-700 px-3 py-2 font-medium text-white"
@@ -68,19 +72,21 @@ const Slide = (props: any) => {
     const direction = props.direction == 'left' ? -1 : 1;
     const translateX = useTransform(props.progress, [0, 1], [250 * direction, -250 * direction]);
 
+
+    const i = props.images;
     return (
         <motion.div style={{ x: translateX, left: props.left }}
             className={`relative flex mt-5  ${props.direction == 'left' ? 'justify-end items-end ' : ' justify-start'} gap-5 whitespace-nowrap`}>
 
-            {props.images.map((index: any) => (
-                <div key={index}>
+            {i.map((index: any, j: number) => (
+                <div key={index} className="">
                     <div className={'flex justify-end mt-auto items-end py-1'}>
                         <Image
                             src={index.src}
                             alt=""
                             width={1280}
                             height={780}
-                            className="mt-auto object-cover"
+                            className={`object-cover object-center ${j % 2 == 0 ? ' aspect-[16/11]' : ' aspect-square'}`}
                         />
                     </div>
                 </div>
@@ -111,6 +117,14 @@ const CtaDefaults: Props = {
             alt: "sdf"
         },
         {
+            src: "/images/rafting/r05.jpg",
+            alt: "sdf"
+        },
+        {
+            src: "/images/rafting/r04.jpg",
+            alt: "sdf"
+        },
+        {
             src: "/images/rafting/r06.jpg",
             alt: "sdf"
         },
@@ -119,9 +133,45 @@ const CtaDefaults: Props = {
             alt: "sdf"
         },
         {
-            src: "/images/rafting/r02.jpg",
+            src: "/images/rafting/r06.jpg",
             alt: "sdf"
-        }
+        },
+        {
+            src: "/images/rafting/r07.jpg",
+            alt: "sdf"
+        },
+        {
+            src: "/images/rafting/r08.jpg",
+            alt: "sdf"
+        },
+        {
+            src: "/images/rafting/r09.jpg",
+            alt: "sdf"
+        },
+        {
+            src: "/images/rafting/r10.jpg",
+            alt: "sdf"
+        },
+        {
+            src: "/images/rafting/r11.jpg",
+            alt: "sdf"
+        },
+        {
+            src: "/images/rafting/r12.jpg",
+            alt: "sdf"
+        },
+        {
+            src: "/images/rafting/r13.jpg",
+            alt: "sdf"
+        },
+        {
+            src: "/images/rafting/r14.jpg",
+            alt: "sdf"
+        },
+        {
+            src: "/images/rafting/r15.jpg",
+            alt: "sdf"
+        },
     ]
 };
 
