68f2c4963d7d9f6483df45ea1926f48264e2e9f1
in-deepaknegi <negideepak.in@gmail.com>
2024-11-08 12:47:53 +0530

    misc

--- components/routes/home/hero.tsx
+++ components/routes/home/hero.tsx
@@ -9,8 +9,8 @@ const Hero: React.FC<HeroProps> = (props) => {
         ...props,
     };
     return (
-        <section className="relative h-screen font-insr">
-            <div className="absolute size-full">
+        <section className="relative h-[110vh] font-insr">
+            <div className="absolute size-full z-50">
                 <div className="flex h-full w-full items-center justify-center">
                     <div className="flex flex-col border p-4 text-center backdrop-blur-lg">
                         <video
@@ -21,10 +21,10 @@ const Hero: React.FC<HeroProps> = (props) => {
                             className="aspect-square w-[25rem] object-cover"
                         />
 
-                        <h2 className="mt-5 font-oggr text-4xl text-black">
+                        <h2 className="mt-5 font-oggr  text-4xl text-white">
                             Sea hawk adventures
                         </h2>
-                        <p className="max-w-sm text-sm text-black">
+                        <p className="max-w-sm text-sm text-white">
                             Kōzōwood engineered this ambitious project exclusively with mixed
                             mass timber construction techniques.
                         </p>
@@ -33,27 +33,27 @@ const Hero: React.FC<HeroProps> = (props) => {
             </div>
             <div className="m-auto h-full max-w-full">
                 <div className="flex h-full flex-row">
-                    <div className="relative mt-32 flex w-full flex-col pl-10">
-                        <h1 className="font-oggr text-8xl">
+                    <div className="relative pt-32 flex w-full flex-col mb-20 bg-[#0c0e17] text-white">
+                        <h1 className="font-oggr text-8xl border-t border-b py-4 pl-10 border-gray-400/30">
                             Sea hawk <br />
                             adventures
                         </h1>
 
-                        <p className="mt-5 text-xl text-gray-600">
+                        <p className="py-5 text-xl text-gray-600 pl-10 border-b border-gray-400/30">
                             Explore Rafting Experience in Rishikesh.
                         </p>
 
                         <Link
-                            className="mb-[4%] mt-auto flex items-center font-mono text-sm"
+                            className="mb-[4%] mt-auto flex gap-2 items-center font-mono text-sm"
                             href={"https://maps.app.goo.gl/eodGFNzZWrLAusB77"}
                             target="_blank"
                         >
                             <Image
-                                src={"/images/svg/map.svg"}
+                                src={"/images/home/google-maps.png"}
                                 alt=""
                                 width={1080}
                                 height={680}
-                                className="w-8"
+                                className="w-5"
                             />
                             30.137240, 78.388864
                         </Link>
--- components/routes/home/services.tsx
+++ components/routes/home/services.tsx
@@ -24,116 +24,135 @@ const Services: React.FC<ServicesProps> = (props) => {
 
 
     return (
-        <section className='relative h-full py-28 font-insr bg-white'>
+        <>
 
-            <div className='mx-auto max-w-full md:max-w-[90%] px-10'>
-                <div className='flex flex-col items-center justify-center text-center'>
-                    <h2 className='font-oggr text-5xl'>
-                        {heading}
-                    </h2>
-                    <p className='text-xl text-gray-600 mt-5 max-w-2xl'>
-                        If you have a vision, we will build it. <br />From modular to custom projects, all perfectly sustainable.
-                    </p>
+            <div className='h-[30vh]'>
+                00
+            </div>
+
+            <section className='relative h-full font-insr bg-[#0c0e17] text-white'>
+
+                <div className='absolute size-full flex justify-center z-0'>
+                    <div className='flex max-w-[90%] px-10 items-center justify-between size-full'>
+                        <div className='bg-gray-400/30 w-[1px] h-full' />
+                        <div className='bg-gray-400/30 w-[0.5px] h-full' />
+                        <div className='bg-gray-400/30 w-[0.5px] h-full' />
+                        <div className='bg-gray-400/30 w-[0.5px] h-full' />
+                        <div className='bg-gray-400/30 w-[0.5px] h-full' />
+                        <div className='bg-gray-400/30 w-[1px] h-full' />
+                    </div>
+
                 </div>
 
-                <div className='mt-12'>
-                    <div className="flex space-x-7">
-                        {services?.map((service, i) => (
-                            <div
-                                key={i}
-                                className="group flex w-full bg-dusk-100/40 backdrop-blur-sm  flex-col transition-all relative duration-700 overflow-hidden border p-4 h-full">
+                <div className='mx-auto max-w-full relative md:max-w-[90%] px-10 py-28 '>
+                    <div className='flex flex-col items-center justify-center text-center'>
+                        <h2 className='font-oggr text-6xl'>
+                            {heading}
+                        </h2>
+                        <p className='text-xl text-gray-300 mt-5 max-w-2xl'>
+                            If you have a vision, we will build it. <br />From modular to custom projects, all perfectly sustainable.
+                        </p>
+                    </div>
 
-                                <div className=' overflow-hidden'>
-                                    <Image
-                                        src={service.thumbnail.src}
-                                        alt={service.thumbnail.alt}
-                                        width={1080}
-                                        height={680}
-                                        className='object-cover aspect-[16/12] group-hover:scale-105 transition-all duration-300 ease-in'
-                                    />
-                                </div>
+                    <div className='mt-12'>
+                        <div className="flex gap-8">
+                            {services?.map((service, i) => (
+                                <div
+                                    key={i}
+                                    className="group flex w-full bg-white backdrop-blur-sm  flex-col transition-all relative duration-700 overflow-hidden border border-dawn-600 p-4 h-full">
 
-                                <div className='pt-5 flex flex-col justify-between text-black h-full'>
-
-                                    <h2 className="text-left font-oggr tracking-wide whitespace-nowrap text-3xl">
-                                        {service.title}
-                                    </h2>
-
-                                    <div className='flex flex-col gap-1 text-sm mt-5 border p-2'>
-                                        <div className='flex items-center gap-4 text-black'>
-                                            <div>
-                                                <Image
-                                                    src={"/images/home/google-calendar.png"}
-                                                    alt=''
-                                                    width={1080}
-                                                    height={680}
-                                                    className='w-4'
-                                                />
-                                            </div>
-                                            <div>
-                                                Timing: Every Day at 9:00 AM and 2:00 PM
-                                            </div>
-                                        </div>
-                                        <div className='flex items-center gap-4 text-black'>
-                                            <div>
-                                                <Image
-                                                    src={"/images/home/google-maps.png"}
-                                                    alt=''
-                                                    width={1080}
-                                                    height={680}
-                                                    className='w-4'
-                                                />
-                                            </div>
-                                            <div>
-                                                Location: Marine Drive - Shivpuri
-                                            </div>
-                                        </div>
-                                        <div className='flex items-center gap-4 text-black'>
-                                            <div>
-                                                <Hourglass className='size-4' />
-                                            </div>
-                                            <div>
-                                                Duration: 1.5 hours
-                                            </div>
-                                        </div>
-
-
+                                    <div className=' overflow-hidden'>
+                                        <Image
+                                            src={service.thumbnail.src}
+                                            alt={service.thumbnail.alt}
+                                            width={1080}
+                                            height={680}
+                                            className='object-cover aspect-[16/12] group-hover:scale-105 transition-all duration-300 ease-in'
+                                        />
                                     </div>
 
-                                    <p className="mt-5 text-3xl text-black">₹ 620
-                                        <span className="text-lg text-gray-500">/person</span>
-                                    </p>
+                                    <div className='pt-5 flex flex-col justify-between text-black h-full'>
 
-                                    <div className='mt-2'>
-                                        <p className={`text-[0.95rem] mt-2 w-fit ${lines[i] ? '' : 'line-clamp-2'} text-dawn-800`}>
-                                            {service.description}
+                                        <h2 className="text-left font-oggr tracking-wide whitespace-nowrap text-3xl">
+                                            {service.title}
+                                        </h2>
+
+                                        <div className='flex flex-col gap-1 text-sm mt-5 border border-dawn-600 p-2'>
+                                            <div className='flex items-center gap-4 text-black'>
+                                                <div>
+                                                    <Image
+                                                        src={"/images/home/google-calendar.png"}
+                                                        alt=''
+                                                        width={1080}
+                                                        height={680}
+                                                        className='w-4'
+                                                    />
+                                                </div>
+                                                <div>
+                                                    Timing: Every Day at 9:00 AM and 2:00 PM
+                                                </div>
+                                            </div>
+                                            <div className='flex items-center gap-4 text-black'>
+                                                <div>
+                                                    <Image
+                                                        src={"/images/home/google-maps.png"}
+                                                        alt=''
+                                                        width={1080}
+                                                        height={680}
+                                                        className='w-4'
+                                                    />
+                                                </div>
+                                                <div>
+                                                    Location: Marine Drive - Shivpuri
+                                                </div>
+                                            </div>
+                                            <div className='flex items-center gap-4 text-black'>
+                                                <div>
+                                                    <Hourglass className='size-4' />
+                                                </div>
+                                                <div>
+                                                    Duration: 1.5 hours
+                                                </div>
+                                            </div>
+
+
+                                        </div>
+
+                                        <p className="mt-5 text-3xl text-black">₹ 620
+                                            <span className="text-lg text-gray-500">/person</span>
                                         </p>
-                                        <button
-                                            onClick={() => toggleLine(i)}
-                                            className='text-blue-700'
-                                        >
-                                            {lines[i] ? "read less..." : "read more..."}
 
-                                        </button>
+                                        <div className='mt-2'>
+                                            <p className={`text-[0.95rem] mt-2 w-fit ${lines[i] ? '' : 'line-clamp-2'} text-dawn-800`}>
+                                                {service.description}
+                                            </p>
+                                            <button
+                                                onClick={() => toggleLine(i)}
+                                                className='text-blue-700'
+                                            >
+                                                {lines[i] ? "read less..." : "read more..."}
+
+                                            </button>
+                                        </div>
+
+
+                                        <div>
+
+                                        </div>
+
+                                        <Link href={"#"} className=' max-w-[90%] mx-auto mt-5 bg-[#0c0e17] text-center w-full text-white whitespace-nowrap px-4 py-2'>
+                                            View Details
+                                        </Link>
                                     </div>
-
-
-                                    <div>
-
-                                    </div>
-
-                                    <Link href={"#"} className=' max-w-[90%] mx-auto mt-5 bg-blue-700 rounded-full text-center w-full text-white whitespace-nowrap px-4 py-2'>
-                                        View Details
-                                    </Link>
                                 </div>
-                            </div>
-                        ))}
+                            ))}
 
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section >
+            </section >
 
+        </>
 
     );
 };
