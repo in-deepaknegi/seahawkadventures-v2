d9850b283559a13a442b90c5a7671c7d05aaf046
in-deepaknegi <negideepak.in@gmail.com>
2025-01-11 22:49:58 +0530

    temp update

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Hero from "@/components/routes/home/hero";
 import Footer from "@/components/globals/footer";
 import CTASection from "@/components/globals/cta";
 import BlogSection from "@/components/globals/blog";
-import { ArrowUpFromDot } from "lucide-react";
+import { ArrowUpFromDot, UserX } from "lucide-react";
 import Package from "@/components/routes/home/package/packages";
 
 export default function Home() {
@@ -25,7 +25,7 @@ export default function Home() {
 
     return (
         <>
-            <Navbar />
+            {/* <Navbar />
             <main>
                 <Hero />
                 <Package/>
@@ -44,7 +44,22 @@ export default function Home() {
                 >
                     <ArrowUpFromDot className="size-5"/>
                 </button>
-            </div>
+            </div> */}
+            <main className="relative h-screen font-insr">
+                <div className="absolute -z-20 h-full w-full bg-white"></div>
+
+                <div className="mx-auto my-auto flex h-full w-full max-w-[80%] flex-col items-center gap-0 align-middle">
+                    <div className="mb-auto mt-10 flex flex-col items-center text-center md:mt-5">
+                        <h1 className="text-3xl font-semibold flex items-center gap-2">
+                           <UserX className="text-red-500 size-12"/> Hoisting Expired
+                        </h1>
+                        <p className="text-black mt-3">
+                            This account has been expired.
+                            Contact your hoisting provider for more information.
+                        </p>
+                    </div>
+                </div>
+            </main>
         </>
     );
 }
